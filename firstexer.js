393bd445437efb19c909d1d553531e4d7b392bfa
Franco Grillo <francoggrillo@gmail.com>
2022-12-01 19:41:17 -0300

    first commit oh

---
+++ firstexer.js
@@ -0,0 +1,15 @@
+const boton1= document.getElementById("unoinput");
+var boton2= document.querySelector("#dosinput");
+const calcular= document.querySelector("#calcular");
+const parrafo= document.querySelector("#fgg");
+
+function calculaSuma(){
+    const res=parseInt(boton1.value )+ parseInt(unoinput.value)
+    
+fgg.innerText="el restultado de la suma es :  " +res;
+
+};
+function alert(){
+const res=parseInt(dosinput.value )+ parseInt(unoinput.value);
+return prompt(res)}
+
